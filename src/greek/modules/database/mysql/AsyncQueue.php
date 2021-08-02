71ea6b9f287fe644e58abc756fff5a9869dd7af0
zOmArRD <omarsena25@gmail.com>
2021-08-02 02:53:05 -0400

    A Mysql handler has been implemented

---
+++ src/greek/modules/database/mysql/AsyncQueue.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * Created by PhpStorm
+ *
+ * User: zOmArRD
+ * Date: 1/8/2021
+ *
+ * Copyright © 2021 - All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace greek\modules\database\mysql;
+
+use greek\network\config\Settings;
+use pocketmine\Server;
+
+class AsyncQueue
+{
+    /** @var array  */
+    private static array $callbacks = [], $values = [];
+
+    /**
+     * @param AsyncQuery $asyncQuery
+     * @param callable|null $callbackFunction
+     * @param array|null $valuesToPass
+     */
+    static public function submitQuery(AsyncQuery $asyncQuery, ?callable $callbackFunction = null, ?array $valuesToPass = null): void
+    {
+        self::$callbacks[spl_object_hash($asyncQuery)] = $callbackFunction;
+        self::$values[spl_object_hash($asyncQuery)] = $valuesToPass;
+        $asyncQuery->host = Settings::$database['host'];
+        $asyncQuery->user = Settings::$database['user'];
+        $asyncQuery->password = Settings::$database['password'];
+        $asyncQuery->database = Settings::$database['database'];
+        Server::getInstance()->getAsyncPool()->submitTask($asyncQuery);
+    }
+
+    /**
+     * @param AsyncQuery $asyncQuery
+     */
+    static public function activateCallback(AsyncQuery $asyncQuery): void
+    {
+        $callable = self::$callbacks[spl_object_hash($asyncQuery)] ?? null;
+        $values = self::$values[spl_object_hash($asyncQuery)] ?? null;
+        if (is_callable($callable)) $callable($asyncQuery["rows"], $values);
+    }
+}
---
+++ src/greek/modules/database/mysql/query/SelectQuery.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Created by PhpStorm
+ *
+ * User: zOmArRD
+ * Date: 1/8/2021
+ *
+ * Copyright © 2021 - All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace greek\modules\database\mysql;
+
+use Exception;
+use greek\Loader;
+use mysqli;
+use pocketmine\Server;
+
+class SelectQuery extends AsyncQuery
+{
+    public $rows;
+
+    public string $query;
+
+    public function __construct(string $sqlQuery)
+    {
+        $this->query = $sqlQuery;
+    }
+
+    public function query(mysqli $mysqli): void
+    {
+        $result = $mysqli->query($this->query);
+        $rows = [];
+        try {
+            if ($result !== false) {
+                while ($row = $result->fetch_assoc()) {
+                    $rows[] = $row;
+                }
+                $this->rows = serialize($rows);
+            }
+        } catch (Exception $exception) {
+            var_dump($exception->getMessage());
+        }
+    }
+
+    public function onCompletion(Server $server)
+    {
+        if ($this->rows === null) Loader::$logger->error("Error while executing query. Please check database settnigs and try again.");
+        $this->rows = unserialize($this->rows);
+        parent::onCompletion($server);
+    }
+}
